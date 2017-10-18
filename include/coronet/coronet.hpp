c57674ce5b1b64450730d1be219784617d284c3a
Eric Niebler <eniebler@boost.org>
2017-10-18 14:26:15 -0700

    Empty shell

---
+++ include/coronet/coronet.hpp
@@ -0,0 +1,22 @@
+// coronet - An experimental networking library that supports both the
+//           Universal Model of the Networking TS and the coroutines of
+//           the Coroutines TS.
+//
+//  Copyright Eric Niebler 2017
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/coronet
+//
+#ifndef CORONET_CORONET_HPP
+#define CORONET_CORONET_HPP
+
+namespace coronet
+{
+    
+}
+
+#endif
---
+++ scratch/scratch.cpp
@@ -0,0 +1,22 @@
+// coronet - An experimental networking library that supports both the
+//           Universal Model of the Networking TS and the coroutines of
+//           the Coroutines TS.
+//
+//  Copyright Eric Niebler 2017
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/coronet
+//
+
+#include <coronet/coronet.hpp>
+#include <cppcoro/task.hpp>
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello, coronet!" << std::endl;
+}
